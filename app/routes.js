05147d2e2d2e96506150e6632c38a6eceb830bdd
Vince Gopez <vince.gopez@cbd.int>
2015-08-06 09:28:42 -0400

    Fixed line endings.

--- app/routes.js
+++ app/routes.js
@@ -10,7 +10,6 @@ define(['app', 'providers/extended-route-provider'], function(app) { 'use strict
         .when('/statistics',                    {templateUrl: 'app/views/stats.html',         resolveController: true })
         .when('/printshop',                     {templateUrl: 'app/views/printshop.html',     resolveController: true })
         .when('/authorization',                 {templateUrl: 'app/views/authorization.html', resolveController: true })
-
         .when('/badge/:badge',                  {templateUrl: 'app/views/index-id.html',      resolveController: true })
         .when('/404',                           {templateUrl: 'app/views/404.html',           resolveUser: false })
         .otherwise({redirectTo: '/404'});
