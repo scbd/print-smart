58bfe1cd78cbbd60dafb62e6a553f770650bb0da
Stephane Bilodeau <bilodeaustephane@gmail.com>
2019-08-27 08:00:33 +0300

    Missing tag number

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-docker build -t scbd/printsmart-distribution git@github.com:scbd/printsmart-distribution.git
+if [ -z "$1" ]
+then
+ echo Missing tag number
+ exit -1;
+fi
 
-docker push scbd/printsmart-distribution
+
+docker build -t scbd/printsmart-distribution:$1 git@github.com:scbd/printsmart-distribution.git && \
+docker push scbd/printsmart-distribution:$1
